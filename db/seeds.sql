fb3c299621c218ff5744bedcab57bc7e94203278
Adrian Fahrer <acfahrer@gmail.com>
2017-08-28 16:25:46 -0400

    upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+USE `burgers_db`;
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Mushroom Swiss', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Double with Cheese', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Slider', false, CURRENT_TIMESTAMP);
