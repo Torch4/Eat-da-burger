69867248b9f3009c705d15b6cddbf59dfb104795
Adrian Fahrer <acfahrer@gmail.com>
2017-08-29 05:24:12 -0400

    upload

--- config/orm.js
+++ config/orm.js
@@ -2,28 +2,23 @@ var connection = require('../config/connection.js');
 
 function printQuestionMarks(num) {
 	var arr = [];
-
 	for (var i = 0; i < num; i++) {
 		arr.push('?');
 	}
-
 	return arr.toString();
 }
 
 function objToSql(ob) {
 	var arr = [];
-
 	for (var key in ob) {
 		if (ob.hasOwnProperty(key)) {
 			arr.push(key + '=' + ob[key]);
 		}
 	}
-
 	return arr.toString();
 }
 
 var orm = {
-
 	selectAll: function(tableInput, cb) {
 		var queryString = 'SELECT * FROM ' + tableInput + ';';
 		connection.query(queryString, function(err, result) {
@@ -35,7 +30,6 @@ var orm = {
 	
 	insertOne: function(table, cols, vals, cb) {
 		var queryString = 'INSERT INTO ' + table;
-
 		queryString += ' (';
 		queryString += cols.toString();
 		queryString += ') ';
